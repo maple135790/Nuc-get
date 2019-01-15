e714b2a577533b1034fc421e079c7074b38ef4bd
KennethHung <maple135790@gmail.com>
2019-01-16 05:55:01 +0800

    Complete get slice on srcFile

--- nucGet.py
+++ nucGet.py
@@ -1,9 +1,23 @@
 def getNuc(file,start,end):
     return file[start:end]
 
+def posDictCreate(posFilename):
+    with open(posFilename,"r") as pfr:
+        k =pfr.read()
+        k =k.splitlines()
+        for i in range(len(k)):
+            m =k[i].split(",")[1:3]
+            posDict[m[0]] =m[1]
+    return posDict
+
 if __name__ == "__main__":
-    pos =dict()
-    with open("aa.fa","r") as fr:
-        k =fr.read()
-        sat =getNuc(k,10,15)
-    print(sat)
+    posDict =dict()
+    srcFile ="aa.fa"
+    posFile ="pos.txt"
+    posDict =posDictCreate(posFile)
+
+    with open(srcFile,"r") as sfr:
+        k =sfr.read()
+        for start,end in posDict.items():
+            sat =getNuc(k,int(start),int(end))
+            print(sat)
