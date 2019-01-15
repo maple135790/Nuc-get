406273225a4b236f1378483688d42f77b168c413
KennethHung <maple135790@gmail.com>
2019-01-16 04:42:05 +0800

    Initial Commit

--- nucGet.py
+++ nucGet.py
@@ -1,6 +1,8 @@
 def getNuc(file,start,end):
     return file[start:end]
+
 if __name__ == "__main__":
+    pos =dict()
     with open("aa.fa","r") as fr:
         k =fr.read()
         sat =getNuc(k,10,15)
